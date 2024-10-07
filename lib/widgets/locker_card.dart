ce09ea6b8fc59e46659a96f4742b3221a26fff36
DailyLollipops <clarencemadrigal84@gmail.com>
2024-10-08 03:32:37 +0800

    Update locker card to show pending details and update status

--- lib/widgets/locker_card.dart
+++ lib/widgets/locker_card.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LockerStatus {
   static const String available = "available";
   static const String unavailable = "unavailable";
   static const String maintenance = "maintenance";
+
+  static List<String> getStatusList() {
+    return [available, unavailable, maintenance];
+  }
 }
 
 class LockerCard extends StatefulWidget {
@@ -12,6 +17,7 @@ class LockerCard extends StatefulWidget {
   final String text;
   final String status;
   final Function()? onPressed;
+  final Function(String newStatus)? onStatusChanged;
 
   const LockerCard({
     super.key,
@@ -20,6 +26,7 @@ class LockerCard extends StatefulWidget {
     required this.text,
     required this.status,
     this.onPressed,
+    this.onStatusChanged,
   });
 
   @override
@@ -27,11 +34,14 @@ class LockerCard extends StatefulWidget {
 }
 
 class _LockerCardState extends State<LockerCard> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
   late Color cardColor;
+  String? selectedStatus;
 
   @override
   void initState() {
     cardColor = _getColorForStatus(widget.status);
+    selectedStatus = widget.status;
     super.initState();
   }
 
@@ -51,8 +61,9 @@ class _LockerCardState extends State<LockerCard> {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        // Add functionality for compartment
+      onPressed: () async {
+        final transaction = await getPendingTransaction(widget.text);
+        showTransactionDialog(context, transaction);
       },
       style: ElevatedButton.styleFrom(
         backgroundColor: cardColor,
@@ -70,5 +81,176 @@ class _LockerCardState extends State<LockerCard> {
         ),
       ),
     );
+  }
+
+  Future<Map<String, dynamic>?> getPendingTransaction(
+      String compartmentId) async {
+    try {
+      QuerySnapshot querySnapshot = await db
+          .collection('transactions')
+          .where('compartment_id', isEqualTo: compartmentId)
+          .where('status', isEqualTo: 'pending')
+          .limit(1)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return querySnapshot.docs.first.data() as Map<String, dynamic>;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print("Error fetching pending transaction: $e");
+      return null;
+    }
+  }
+
+  void showTransactionDialog(
+      BuildContext context, Map<String, dynamic>? transaction) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text(transaction == null
+                  ? "No Pending Transaction"
+                  : "Transaction Details"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (transaction == null)
+                    const Text(
+                      "No pending transaction for this compartment.",
+                      textAlign: TextAlign.center,
+                    )
+                  else
+                    Column(
+                      children: [
+                        buildDetailRow("Transaction Type:",
+                            cleanTransactionType(transaction["item"] ?? "N/A")),
+                        buildDetailRow("Item Category:",
+                            transaction["item_category"] ?? "N/A"),
+                        buildDetailRow(
+                            "Sender:", transaction["sender"] ?? "N/A"),
+                        buildDetailRow("Sender Contact:",
+                            transaction["sender_contact"] ?? "N/A"),
+                        buildDetailRow(
+                            "Receiver:", transaction["receiver"] ?? "N/A"),
+                        buildDetailRow("Receiver Contact:",
+                            transaction["receiver_contact"] ?? "N/A"),
+                        buildDetailRow(
+                            "Dropoff Date:",
+                            (transaction["dropoff_at"] != null
+                                ? (transaction["dropoff_at"] as Timestamp)
+                                    .toDate()
+                                    .toString()
+                                : "N/A")),
+                        buildDetailRow(
+                            "Received Date:",
+                            (transaction["received_at"] != null
+                                ? (transaction["received_at"] as Timestamp)
+                                    .toDate()
+                                    .toString()
+                                : "N/A")),
+                      ],
+                    ),
+                  const SizedBox(height: 16),
+                  const Text("Change Compartment Status:"),
+                  DropdownButton<String>(
+                    value: selectedStatus,
+                    items: LockerStatus.getStatusList().map((String status) {
+                      return DropdownMenuItem<String>(
+                        value: status,
+                        child: Text(status),
+                      );
+                    }).toList(),
+                    onChanged: (newStatus) {
+                      setState(() {
+                        selectedStatus = newStatus;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Close"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (selectedStatus != null) {
+                      await updateCompartmentStatus(
+                          widget.text, selectedStatus!);
+                      setState(() {
+                        cardColor = _getColorForStatus(selectedStatus!);
+                      });
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: const Text("Save"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  String cleanTransactionType(String transactionType) {
+    switch (transactionType) {
+      case "regular":
+        return "Regular";
+      case "lost_and_found":
+        return "Lost and Found";
+      default:
+        return "Unknown";
+    }
+  }
+
+  Widget buildDetailRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              value,
+              textAlign: TextAlign.right,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> updateCompartmentStatus(
+      String compartmentId, String newStatus) async {
+    try {
+      await db.collection('compartments').doc(compartmentId).update(
+          {'status': newStatus, 'updated_at': FieldValue.serverTimestamp()});
+      print("Compartment status updated successfully.");
+
+      setState(() {
+        cardColor = _getColorForStatus(newStatus);
+      });
+
+      if (widget.onStatusChanged != null) {
+        widget.onStatusChanged!(newStatus);
+      }
+    } catch (e) {
+      print("Error updating compartment status: $e");
+    }
   }
 }
