6144e04bea2c0be542683fd668a32fbae5f24107
DailyLollipops <clarencemadrigal84@gmail.com>
2024-10-08 02:33:55 +0800

    Update transaction page

--- lib/pages/transactions.dart
+++ lib/pages/transactions.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_3/widgets/drawer.dart';
 import 'package:flutter_application_3/widgets/drawer_button.dart';
+import 'package:flutter_application_3/widgets/transaction_card.dart';
 
 class Transactions extends StatefulWidget {
   const Transactions({super.key});
@@ -16,23 +17,100 @@ class _TransactionsState extends State<Transactions> {
   FirebaseFirestore db = FirebaseFirestore.instance;
   bool isLoading = false;
   List<Map<String, dynamic>> transactions = [];
+  List<Map<String, dynamic>> filteredTransactions = [];
+
+  String selectedTransactionType = 'All';
+  DateTime? startDate;
+  DateTime? endDate;
 
   @override
   void initState() {
-    isLoading = true;
+    super.initState();
+    fetchTransactions();
+  }
+
+  void fetchTransactions() {
+    setState(() {
+      isLoading = true;
+    });
     final transactionsRef = db.collection("transactions");
     transactionsRef.get().then(
       (querySnapshot) {
         setState(() {
-          for (var docSnapshot in querySnapshot.docs) {
-            transactions.add(docSnapshot.data());
-          }
+          transactions = querySnapshot.docs
+              .map((docSnapshot) => docSnapshot.data() as Map<String, dynamic>)
+              .toList();
+          filteredTransactions = transactions;
         });
         isLoading = false;
       },
       onError: (e) => print("Error getting document: $e"),
     );
-    super.initState();
+  }
+
+  void filterTransactions(String transactionType) {
+    setState(() {
+      selectedTransactionType = transactionType;
+      applyFilters();
+    });
+  }
+
+  void applyFilters() {
+    setState(() {
+      filteredTransactions = transactions.where((transaction) {
+        bool matchesType = selectedTransactionType == 'All' ||
+            transaction["transaction_type"] == selectedTransactionType;
+        DateTime dropoffDate = transaction["dropoff_at"].toDate();
+        bool matchesDate = true;
+        if (startDate != null) {
+          matchesDate = matchesDate && dropoffDate.isAfter(startDate!);
+        }
+        if (endDate != null) {
+          matchesDate = matchesDate && dropoffDate.isBefore(endDate!);
+        }
+
+        return matchesType && matchesDate;
+      }).toList();
+    });
+  }
+
+  Future<void> _selectStartDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: startDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != startDate) {
+      setState(() {
+        startDate = picked;
+        applyFilters();
+      });
+    }
+  }
+
+  Future<void> _selectEndDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: endDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != endDate) {
+      setState(() {
+        endDate = picked;
+        applyFilters();
+      });
+    }
+  }
+
+  void clearFilters() {
+    setState(() {
+      selectedTransactionType = 'All';
+      startDate = null;
+      endDate = null;
+      filteredTransactions = transactions;
+    });
   }
 
   @override
@@ -86,16 +164,70 @@ class _TransactionsState extends State<Transactions> {
                             ),
                           ),
                         ),
+                        const SizedBox(height: 12),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintText: 'Search',
-                              prefixIcon: const Icon(Icons.search),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: DropdownButton<String>(
+                                  isExpanded: true,
+                                  value: selectedTransactionType,
+                                  items: const [
+                                    DropdownMenuItem(
+                                      value: 'All',
+                                      child: Text('All Transactions'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: 'regular',
+                                      child: Text('Regular'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: 'lost_and_found',
+                                      child: Text('Lost and Found'),
+                                    ),
+                                  ],
+                                  onChanged: (value) {
+                                    if (value != null) {
+                                      filterTransactions(value);
+                                    }
+                                  },
+                                ),
                               ),
-                            ),
+                              const SizedBox(width: 10),
+                              IconButton(
+                                onPressed: clearFilters,
+                                icon: const Icon(Icons.clear_all_outlined),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () => _selectStartDate(context),
+                                child: Text(
+                                  startDate == null
+                                      ? "Select Start Date"
+                                      : DateFormat('MMMM dd, yyyy')
+                                          .format(startDate!),
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () => _selectEndDate(context),
+                                child: Text(
+                                  endDate == null
+                                      ? "Select End Date"
+                                      : DateFormat('MMMM dd, yyyy')
+                                          .format(endDate!),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                         const SizedBox(height: 12),
@@ -104,46 +236,30 @@ class _TransactionsState extends State<Transactions> {
                             controller: _scrollController,
                             child: Column(
                               children: List.generate(
-                                transactions.length,
-                                (index) {
-                                  bool received = transactions[index]
-                                          ["received_at"] !=
-                                      null;
-                                  String lastUpdate = received
-                                      ? DateFormat('MM/dd/yyyy hh:mm a').format(
-                                          transactions[index]["received_at"]
-                                              .toDate())
-                                      : DateFormat('MM/dd/yyyy hh:mm a').format(
-                                          transactions[index]["dropoff_at"]
-                                              .toDate());
-                                  return Card(
-                                    child: ListTile(
-                                      title: Text(transactions[index]
-                                              ["item_category"] ??
-                                          "Unknown"),
-                                      subtitle: Text("Updated: $lastUpdate"),
-                                      trailing: Chip(
-                                        padding: const EdgeInsets.all(1.5),
-                                        label: Text(
-                                          received
-                                              ? 'Completed'
-                                              : '   Pending   ',
-                                          style: const TextStyle(
-                                            fontFamily: 'Inter',
-                                            fontSize: 10,
-                                            color: Colors.white70,
-                                          ),
-                                        ),
-                                        backgroundColor: received
-                                            ? Colors.green
-                                            : Colors.red,
-                                      ),
-                                      onTap: () {
-                                        // Add functionality here
-                                      },
-                                    ),
-                                  );
-                                },
+                                filteredTransactions.length,
+                                (index) => TransactionCard(
+                                  transactionType: filteredTransactions[index]
+                                      ["transaction_type"],
+                                  itemCategory: filteredTransactions[index]
+                                      ["item_category"],
+                                  itemSubCategory: filteredTransactions[index]
+                                      ["item_subcategory"],
+                                  itemDetail: filteredTransactions[index]
+                                      ["item_detail"],
+                                  sender: filteredTransactions[index]["sender"],
+                                  senderContact: filteredTransactions[index]
+                                      ["sender_contact"],
+                                  receiver: filteredTransactions[index]
+                                      ["receiver"],
+                                  receiverContact: filteredTransactions[index]
+                                      ["receiver_contact"],
+                                  dropoffDate: filteredTransactions[index]
+                                          ["dropoff_at"]
+                                      .toDate(),
+                                  receivedDate: filteredTransactions[index]
+                                          ["received_at"]
+                                      ?.toDate(),
+                                ),
                               ),
                             ),
                           ),
---
+++ lib/widgets/transaction_card.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionCard extends StatefulWidget {
+  final String transactionType;
+  final String itemCategory;
+  final String? itemSubCategory;
+  final String? itemDetail;
+  final String sender;
+  final String senderContact;
+  final String? receiver;
+  final String? receiverContact;
+  final DateTime dropoffDate;
+  final DateTime? receivedDate;
+
+  const TransactionCard({
+    super.key,
+    required this.transactionType,
+    required this.itemCategory,
+    required this.sender,
+    required this.senderContact,
+    required this.dropoffDate,
+    this.itemSubCategory,
+    this.itemDetail,
+    this.receiver,
+    this.receiverContact,
+    this.receivedDate,
+  });
+
+  @override
+  State<TransactionCard> createState() => _TransactionCardState();
+}
+
+class _TransactionCardState extends State<TransactionCard> {
+  @override
+  Widget build(BuildContext context) {
+    bool isReceived = widget.receivedDate != null;
+    String lastUpdate = isReceived
+        ? DateFormat('MMMM dd, yyyy')
+            .format(widget.receivedDate ?? DateTime(2000))
+        : DateFormat('MMMM dd, yyyy').format(widget.dropoffDate);
+
+    Color getAvatarColor(String transactionType) {
+      switch (widget.transactionType) {
+        case "regular":
+          return Colors.blue.shade300;
+        case "lost_and_found":
+          return Colors.lime.shade300;
+        default:
+          return Colors.grey.shade300;
+      }
+    }
+
+    String getAvatarInitials(String transactionType) {
+      switch (widget.transactionType) {
+        case "regular":
+          return "R";
+        case "lost_and_found":
+          return "LF";
+        default:
+          return "U";
+      }
+    }
+
+    return Card(
+      child: ListTile(
+        title: Text(widget.itemCategory),
+        subtitle: Text(lastUpdate),
+        leading: CircleAvatar(
+          backgroundColor: getAvatarColor(widget.transactionType),
+          child: Text(getAvatarInitials(widget.transactionType)),
+        ),
+        trailing: Chip(
+          padding: const EdgeInsets.all(1.5),
+          label: Text(
+            isReceived ? 'Completed' : '   Pending   ',
+            style: const TextStyle(
+              fontFamily: 'Inter',
+              fontSize: 10,
+              color: Colors.white70,
+            ),
+          ),
+          backgroundColor: isReceived ? Colors.green : Colors.red,
+        ),
+        onTap: () {
+          _showDetailsDialog(context);
+        },
+      ),
+    );
+  }
+
+  void _showDetailsDialog(BuildContext context) {
+    bool isReceived = widget.receivedDate != null;
+    String status = isReceived ? "Completed" : "Pending";
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Transaction Details'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                if (widget.transactionType == "regular") ...[
+                  buildDetailRow('Item Category:', widget.itemCategory),
+                  buildDetailRow('Status:', status),
+                  buildDetailRow('Sender:', widget.sender),
+                  buildDetailRow('Contact:', widget.senderContact),
+                  if (widget.receiver == null)
+                    buildDetailRow('Receiver:', 'No receiver'),
+                  if (widget.receiver != null)
+                    buildDetailRow('Receiver:', widget.receiver!),
+                  if (widget.receiverContact == null)
+                    buildDetailRow('Contact:', 'No contact'),
+                  if (widget.receiverContact != null)
+                    buildDetailRow('Contact:', widget.receiverContact!),
+                  buildDetailRow(
+                      'Dropoff Date:',
+                      DateFormat('MMMM dd, yyyy hh:mm a')
+                          .format(widget.dropoffDate)),
+                  if (widget.receivedDate == null)
+                    buildDetailRow('Received Date:', 'Not received'),
+                  if (widget.receivedDate != null)
+                    buildDetailRow(
+                        'Received Date:',
+                        DateFormat('MMMM dd, yyyy hh:mm a')
+                            .format(widget.receivedDate!)),
+                ] else if (widget.transactionType == "lost_and_found") ...[
+                  buildDetailRow('Item Category:', widget.itemCategory),
+                  if (widget.itemSubCategory != null)
+                    buildDetailRow('Sub-category:', widget.itemSubCategory!),
+                  if (widget.itemDetail != null)
+                    buildDetailRow('Detail:', widget.itemDetail!),
+                  buildDetailRow('Status:', status),
+                  buildDetailRow('Sender:', widget.sender),
+                  buildDetailRow('Contact:', widget.senderContact),
+                  if (widget.receiver != null)
+                    buildDetailRow('Receiver:', widget.receiver!),
+                  if (widget.receiver == null)
+                    buildDetailRow('Receiver:', 'No receiver'),
+                  if (widget.receiverContact == null)
+                    buildDetailRow('Contact:', 'No contact'),
+                  if (widget.receiverContact != null)
+                    buildDetailRow('Contact:', widget.receiverContact!),
+                  buildDetailRow(
+                      'Dropoff Date:',
+                      DateFormat('MMMM dd, yyyy hh:mm a')
+                          .format(widget.dropoffDate)),
+                  if (widget.receivedDate == null)
+                    buildDetailRow('Received Date:', 'Not received'),
+                  if (widget.receivedDate != null)
+                    buildDetailRow(
+                        'Received Date:',
+                        DateFormat('MMMM dd, yyyy hh:mm a')
+                            .format(widget.receivedDate!)),
+                ] else ...[
+                  const Text('Invalid transaction type'),
+                ]
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget buildDetailRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Text(
+              label,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Text(
+              value,
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
